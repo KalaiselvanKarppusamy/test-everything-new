b0f27407b15017a4c7ff2aebd2ca314cf6e71dcd
Kalaiselvan Karuppusamy <kalai.uvan@gmail.com>
2014-08-27 06:22:24 -0700

    added calculator test

---
+++ Calculator.php
@@ -0,0 +1,23 @@
+<?php
+
+class Calculator
+{
+    public function push($num)
+    {
+        if (!isset($this->items)) {
+            $this->items = array($num);
+        } else {
+            array_push($this->items, $num);
+        }
+    }
+
+    public function add()
+    {
+        $this->result = array_sum($this->items);
+    }
+
+    public function result()
+    {
+        return $this->result;
+    }
+}
--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -7,12 +7,9 @@ use Behat\Behat\Context\ClosuredContextInterface,
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 
-//
-// Require 3rd-party libraries here:
-//
-//   require_once 'PHPUnit/Autoload.php';
-//   require_once 'PHPUnit/Framework/Assert/Functions.php';
-//
+   require_once 'PHPUnit/Autoload.php';
+   require_once 'PHPUnit/Framework/Assert/Functions.php';
+   require_once 'Calculator.php';
 
 /**
  * Features context.
@@ -21,13 +18,39 @@ class FeatureContext extends BehatContext
 {
     /**
      * Initializes context.
-     * Every scenario gets its own context object.
+     * Every scenario gets it's own context object.
      *
-     * @param array $parameters context parameters (set them up through behat.yml)
+     * @param   array   $parameters     context parameters (set them up through behat.yml)
      */
     public function __construct(array $parameters)
     {
-        // Initialize your context here
+        if (!isset($this->calculator)) {
+            $this->calculator = new Calculator();
+        }
+    }
+
+    /**
+     * @Given /^I have entered (\d+) into the calculator$/
+     */
+    public function iHaveEnteredIntoTheCalculator($argument1)
+    {
+        $this->calculator->push($argument1);
+    }
+
+    /**
+     * @When /^I press add$/
+     */
+    public function iPressAdd()
+    {
+        $this->calculator->add();
+    }
+
+    /**
+     * @Then /^the result should be (\d+) on the screen$/
+     */
+    public function theResultShouldBeOnTheScreen($argument1)
+    {
+        assertEquals($argument1, $this->calculator->result());
     }
 
     /**
